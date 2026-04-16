a2ac1d538b4da09a7dd0202d093796c85622f8bc
TZHU64 <95006669+TZHU64@users.noreply.github.com>
2026-04-16 13:22:33 +0000

    Deploying to gh-pages from @ TZHU64/tzhu64.github.io@28b3625d47552ed95f6e579d535d657cfe5e9734 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,27 +23,24 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "中文简历下载 | English CV Download",
+        },{id: "nav-resume",
+          title: "resume",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/resume/";
           },
         },{id: "news-winner-of-two-excellence-awards-in-the-2023-konica-minolta-group-global-improvement-competition",
           title: 'Winner of Two Excellence Awards in the 2023 Konica Minolta Group Global Improvement...',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2023-10-01/";
-            },},{id: "news-我代表instrument-systems受邀出席第四届深圳国际mini-micro-led产业链创新发展高峰论坛-并发表题为-晶圆上µled和微型显示器的快速测试-的演讲",
-          title: '我代表Instrument Systems受邀出席第四届深圳国际Mini/Micro LED产业链创新发展高峰论坛，并发表题为《晶圆上µLED和微型显示器的快速测试》的演讲。',
+            },},{id: "news-i-represented-instrument-systems-at-the-4th-shenzhen-international-mini-micro-led-industry-chain-innovation-and-development-summit-forum-giving-a-presentation-titled-quot-rapid-testing-of-µleds-and-microdisplays-on-wafer-quot",
+          title: 'I represented Instrument Systems at the 4th Shenzhen International Mini/Micro LED Industry Chain...',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2023-10-12/";
-            },},{id: "news-我的新书-vr光学测量手册-分为九个章节-全面涵盖了国际标准-测量设备-测量方法以及实际案例研究-书中系统地讲解了如何利用光学测量技术精确测量增强现实-ar-虚拟现实-vr-和混合现实-mr-设备及其组件-本书旨在帮助感兴趣的人士更深入地理解xr技术-同时规范相关术语和技术知识-为行业发展提供有力支持-除实体书外-我将其核心内容同步发布在了github-知乎专栏和csdn专栏",
-          title: '我的新书《VR光学测量手册》分为九个章节，全面涵盖了国际标准、测量设备、测量方法以及实际案例研究。书中系统地讲解了如何利用光学测量技术精确测量增强现实（AR）、虚拟现实（VR）和混合现实（MR）设备及其组件。本书旨在帮助感兴趣的人士更深入地理解XR技术，同时规范相关术语和技术知识，为行业发展提供有力支持。除实体书外，我将其核心内容同步发布在了Github，知乎专栏和CSDN专栏。',
-          description: "",
-          section: "News",},{id: "news-i-authored-the-book-handbook-of-vr-optical-measurement-the-book-is-divided-into-nine-chapters-comprehensively-covering-international-standards-measurement-equipment-measurement-methods-and-real-world-case-studies-it-systematically-explains-how-to-use-optical-measurement-technology-to-precisely-measure-augmented-reality-ar-virtual-reality-vr-and-mixed-reality-mr-devices-and-their-components-this-book-aims-to-help-interested-people-gain-a-deeper-understanding-of-xr-technologies-while-standardizing-related-terminology-and-technical-knowledge-to-provide-strong-support-for-industry-development-in-addition-to-the-physical-book-i-have-also-published-its-core-content-on-github",
+            },},{id: "news-i-authored-the-book-handbook-of-vr-optical-measurement-the-book-is-divided-into-nine-chapters-comprehensively-covering-international-standards-measurement-equipment-measurement-methods-and-real-world-case-studies-it-systematically-explains-how-to-use-optical-measurement-technology-to-precisely-measure-augmented-reality-ar-virtual-reality-vr-and-mixed-reality-mr-devices-and-their-components-this-book-aims-to-help-interested-people-gain-a-deeper-understanding-of-xr-technologies-while-standardizing-related-terminology-and-technical-knowledge-to-provide-strong-support-for-industry-development-in-addition-to-the-physical-book-i-have-also-published-its-core-content-on-github",
           title: 'I authored the book Handbook of VR Optical Measurement. The book is divided...',
           description: "",
           section: "News",},{id: "news-i-was-honored-to-represent-instrument-systems-as-an-invited-speaker-at-the-2025-international-conference-on-display-technology-icdt-2025-where-i-presented-our-research-titled-measurement-of-near-eye-displays-using-a-goniophotometer-and-imaging-light-measurement-device",
@@ -57,6 +54,12 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "news-i-m-honored-to-share-that-i-have-been-appointed-as-a-technical-committee-member-of-the-society-for-information-display-sid-beijing-chapter-in-this-capacity-i-will-actively-contribute-to-two-key-sub-committees-vr-amp-amp-ar-and-display-measurement-this-role-offers-a-valuable-opportunity-to-collaborate-with-industry-and-academic-leaders-in-advancing-display-technologies-i-look-forward-to-helping-shape-innovation-at-the-intersection-of-science-engineering-and-real-world-applications",
           title: 'I’m honored to share that I have been appointed as a Technical Committee...',
+          description: "",
+          section: "News",},{id: "news-i-am-excited-to-announce-the-official-publication-of-the-xr-optical-testing-handbook-isbn-978-7-89569-660-0-this-release-is-a-massive-update-and-complete-rewrite-of-my-2024-book-the-handbook-of-vr-optical-measurement-greatly-expanding-on-the-original-content-to-reflect-the-latest-testing-methodologies-and-advancements-across-the-xr-industry",
+          title: 'I am excited to announce the official publication of the XR Optical Testing...',
+          description: "",
+          section: "News",},{id: "news-i-was-honored-to-represent-instrument-systems-at-the-2026-international-conference-on-display-technology-icdt-2026-where-i-presented-our-three-recently-published-papers-advancing-ar-optics-and-near-eye-display-measurement-our-presented-research-includes-gaze-vs-view-a-framework-for-correlating-dynamic-and-static-measurements-in-ar-optics-colorimetric-characterization-of-see-through-near-eye-displays-and-the-impact-of-waveguide-spectral-filtering-on-ar-color-fidelity",
+          title: 'I was honored to represent Instrument Systems at the 2026 International Conference on...',
           description: "",
           section: "News",},{
         id: 'social-email',
